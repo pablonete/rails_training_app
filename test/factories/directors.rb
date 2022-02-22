b069c8d3cb84e8185841085e40a0ca4fcfccba56
Pablo Núñez <pablonete@github.com>
2022-02-22 18:17:44 +0100

    Add traits.

--- test/factories/directors.rb
+++ test/factories/directors.rb
@@ -2,5 +2,13 @@ FactoryBot.define do
   factory :director do
     name { Faker::Name.name }
     age { rand(100) }
+
+    trait :with_movies do
+      movies { Array.new(3) { association(:movie, director: instance) } }
+    end
+
+    trait :older_than_forty do
+      age { 40 + rand(60) }
+    end
   end
 end
