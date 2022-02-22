3a411adc79298d793d85ed45ecf09a3c1099dccd
Matt Jones <al2o3cr@gmail.com>
2022-02-21 22:01:35 -0500

    extract to model method

--- app/controllers/rereleases_controller.rb
+++ app/controllers/rereleases_controller.rb
@@ -2,9 +2,7 @@ class RereleasesController < ApplicationController
   def create
     movie = Movie.find(params[:movie_id])
 
-    rerelease = movie.dup
-    rerelease.year = params[:year]
-    rerelease.save
+    rerelease = movie.rerelease(params)
 
     redirect_to movie_path(rerelease)
   end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -69,4 +69,24 @@ class MovieTest < ActiveSupport::TestCase
 
     assert_equal 1, count
   end
+
+  test "rerelease does not alter the original movie year" do
+    movie = create(:movie, year: "1992")
+
+    movie.rerelease(year: "2019")
+
+    first_release = Movie.find(movie.id)
+    assert_equal "1992", movie.year
+  end
+
+  test "rerelease creates a copy of the movie with the specified year" do
+    movie = create(:movie, year: "1992")
+
+    assert_difference(-> { Movie.where(title: movie.title).count }, 1) do
+      movie.rerelease(year: "2019")
+    end
+
+    rerelease = Movie.find_by(title: movie.title, year: 2019)
+    assert_equal "2019", rerelease.year
+  end
 end
