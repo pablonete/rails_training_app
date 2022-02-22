cb661090119427cc313cd82d31826c4600b0e2d5
Matt Jones <al2o3cr@gmail.com>
2022-02-21 21:38:39 -0500

    add rereleases controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get "/movies", to: "movies#index", as: "movies"
-  post "/movies", to: "movies#create"
-  get "/movies/new", to: "movies#new", as: "new_movie"
-  get "/movies/:id", to: "movies#show", as: "movie"
-  patch "/movies/:id", to: "movies#update"
-  get "/movies/:id/edit", to: "movies#edit", as: "edit_movie"
-  delete "/movies/:id", to: "movies#destroy"
+  resources :movies do
+    resources :rereleases, only: [:create]
+  end
 
   resources :directors
 end
