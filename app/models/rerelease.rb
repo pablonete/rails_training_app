22320adb58bf44e6194b60eaa68b6bb0ffb152a1
Pablo Núñez <pablonete@github.com>
2022-02-23 20:39:47 +0100

    Use super to save params in Model.

--- app/models/rerelease.rb
+++ app/models/rerelease.rb
@@ -1,26 +1,25 @@
 class Rerelease
   include ActiveModel::Model
-  attr_accessor :original, :params
+
+  attr_reader :original
+  attr_accessor :year
+
   validates_presence_of :year
 
   def initialize(original, params)
     @original = original
-    @params = params
+    super(params)
   end
 
   def id
     @result.id
   end
 
-  def year
-    params[:year]
-  end
-
   def save
     return false unless valid?
 
     @result = original.dup
-    @result.year = params[:year]
+    @result.year = year
     @result.save!
   end
 end
